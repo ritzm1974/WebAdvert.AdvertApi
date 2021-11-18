4fbc24e1218b3c1846a23bdadc09a58e2e8fd6cb
ritzm1974 <36458226+ritzm1974@users.noreply.github.com>
2021-11-17 19:10:01 -0800

    add models and api

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,68 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/adverts")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService ?? throw new ArgumentNullException(nameof(advertStorageService));
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+
+            }
+            catch (KeyNotFoundException)
+            { 
+                return new NotFoundResult();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+                throw;
+            }
+
+            return StatusCode(201,  new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm_Put(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            return new OkResult();
+        }
+    }
+}
